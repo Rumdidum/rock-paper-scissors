1439e4bd56ac08b8fa1af2682fee1e838b27d799
Rumdidum <waldemar.rink2021@gmail.com>
2021-11-28 21:16:57 +0100

    renamed point variables

--- scripts/my-script.js
+++ scripts/my-script.js
@@ -1,10 +1,12 @@
-
+// Started project 28.11.2021 at 14:00pm
+// Working as intended!
 let random_number = Math.floor(3*Math.random())
 
 
-let playerpoints = 0;
-let computerpoints = 0;
+let playerPoints = 0;
+let computerPoints = 0;
 
+// Working as intended!
 function computerPlay() {
     if (random_number == 0) {
         handsign = "rock";
@@ -22,28 +24,28 @@ function playRound(playerSelection, computerSelection) {
     console.log(playerSelection)
     if (playerSelection == "rock") {
         if (computerSelection == "rock") {
-            return playerpoints += 0
+            return playerPoints += 0
         } else if (computerSelection == "paper") {
-            return computerpoints += 1
+            return computerPoints += 1
         } else if (computerSelection == "scissor") {
-            return playerpoints += 1
+            return playerPoints += 1
         }
     } else if (playerSelection == "paper") {
         if (computerSelection == "paper") {
-            return playerpoints += 0
+            return playerPoints += 0
         } else if (computerSelection == "scissor") {
-            return computerpoints += 1
+            return computerPoints += 1
         } else if (computerSelection == "rock") {
-            return playerpoints += 1
+            return playerPoints += 1
         }
 
     } else if (playerSelection == "scissor") {
         if (computerSelection == "scissor") {
-            return playerpoints += 0
+            return playerPoints += 0
         } else if (computerSelection == "rock") {
-            return computerpoints += 1
+            return computerPoints += 1
         } else if (computerSelection == "paper") {
-            return playerpoints += 1
+            return playerPoints += 1
         }
     }
 }
@@ -54,9 +56,9 @@ function game() {
         playRound()
         roundCounter += 1
     }
-    if (playerpoints == 5) {
+    if (playerPoints == 5) {
         console.log("you win")
-    } else if (computerpoints == 5) {
+    } else if (computerPoints == 5) {
         console.log("you lose")
     }
 }
