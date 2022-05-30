39f0094bdc3871df8abc60d33239f08e458822d4
Rumdidum <waldemar.rink2021@gmail.com>
2022-05-30 23:31:44 +0200

    added win, lose, tie function and modified handler

--- scripts/my-script.js
+++ scripts/my-script.js
@@ -11,24 +11,30 @@ const computerChoice = document.querySelector('#computerChoice').firstElementChi
 // playerChoice.textContent = "FUUUUUK!";
 
 function handler(selection) {
-    // this is broken because it loops until the last "scissor"
-    if ((playerPoints || computerPoints) < 5) {
-        playRound(selection);
-    } else {
-        results();
-    }
+    playRound(selection);
+    console.log(`Player: ${playerPoints} | Computer: ${computerPoints}`)
 }
 
 function game() {
+
     gameButtons.forEach((button) => {
+        // Its necessary to use Arrow function with another function "handler" passed to it.
+        // Now its possible to remove the listener.
         button.addEventListener('click', () => {handler(button.id)});
     })
 }
 
-function lose() {
-
-}
 function win() {
+    let theWinner = document.querySelector('#theWinner');
+    theWinner.textContent = "YOU WIN!";
+}
+function lose() {
+  let theWinner = document.querySelector('#theWinner');
+  theWinner.textContent = "YOU LOSE!";
+}
+function tie() {
+    let theWinner = document.querySelector('#theWinner');
+    theWinner.textContent = "IT's A TIE!";
 }
 
 function results() {
@@ -37,14 +43,12 @@ function results() {
     }
     )
     if (playerPoints > computerPoints) {
-        console.log("you win")
+        win();
     } else if (computerPoints > playerPoints) {
-        console.log("you lose")
+        lose();
     } else {
-        console.log("its a tie")
+        tie();
     }
-    console.log(`Your points: ${playerPoints}`)
-    console.log(`computer Points: ${computerPoints}`)
 }
 
 // Generates a random number from 0 to 2 and use it to get a string from choice Array
@@ -55,33 +59,33 @@ function computerPLay() {
 }
 
 function playRound(playerSelection, computerSelection = computerPLay()) {
-    // playerSelection = playerClick();
-    console.log(playerSelection + " some fun");
-    // computerSelection = computerPLay();
-    playerChoice.textContent = playerSelection;
-    computerChoice.textContent = computerSelection;
-    console.log(`Player selection: ${playerSelection} and computer: ${computerSelection}`);
-
-    if (playerSelection == "rock") {
-        if (computerSelection == "paper") {
-            computerPoints += 1
-        } else if (computerSelection == "scissor") {
-            playerPoints += 1
+    if ((playerPoints || computerPoints) < 2) {
+        playerChoice.textContent = playerSelection;
+        computerChoice.textContent = computerSelection;
+    
+        if (playerSelection == "rock") {
+            if (computerSelection == "paper") {
+                computerPoints += 1
+            } else if (computerSelection == "scissor") {
+                playerPoints += 1
+            }
+        } else if (playerSelection == "paper") {
+            if (computerSelection == "scissor") {
+                computerPoints += 1
+            } else if (computerSelection == "rock") {
+                playerPoints += 1
+            }
+        } else if (playerSelection == "scissor") {
+            if (computerSelection == "rock") {
+                computerPoints += 1
+            } else if (computerSelection == "paper") {
+                playerPoints += 1
+            }
         }
-    } else if (playerSelection == "paper") {
-        if (computerSelection == "scissor") {
-            computerPoints += 1
-        } else if (computerSelection == "rock") {
-            playerPoints += 1
-        }
-    } else if (playerSelection == "scissor") {
-        if (computerSelection == "rock") {
-            computerPoints += 1
-        } else if (computerSelection == "paper") {
-            playerPoints += 1
-        }
+        computerRenderPts.textContent = computerPoints;
+        playerRenderPts.textContent = playerPoints;
+    } else {
+        results();
     }
-    computerRenderPts.textContent = computerPoints;
-    playerRenderPts.textContent = playerPoints;
 }
 game()
